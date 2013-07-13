ed2108290efb33f2672e7b610cf10efa7e2f7fac
Andreas Bielk <andreas@bielk.se>
2013-07-13 13:20:42 +0200

    store GIT SHA1 tagfile

--- release.sh
+++ release.sh
@@ -7,6 +7,11 @@ rm -f resources/public.zip
 zip -r resources/public.zip public
 
 echo
+echo "storing GIT SHA1"
+GIT_SHA1=`(git show-ref --head --hash=8 2> /dev/null || echo 00000000) | head -n1`
+echo -n $GIT_SHA1 > ./resources/git-sha1
+
+echo
 echo "creating uberjar"
 LEIN_SNAPSHOTS_IN_RELEASE=y lein uberjar
 
